ccfcdbd83b11b995d66256d38476ef31bd843261
leo.pirringer <leo.pirringer@sz-ybbs.ac.at>
2023-01-19 18:08:00 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        Boolean IsgoingOn = true;
+
+        while (IsgoingOn) {
+            try {
+                System.out.println("Enter the number: ");
+                double n = scanner.nextInt();
+
+                System.out.println("Enter the number: ");
+                double y = scanner.nextInt();
+
+                double z = n / y;
+                System.out.println("Result: " + z);
+            }
+            //Mathematical exception (Devide by zero)
+            catch (ArithmeticException e) {
+                System.out.println("You can't divide by zero idiot");
+            }
+            //Input does not match expected type of data
+            catch(InputMismatchException e){
+                System.out.println("You can't enter a string idiot");
+            }
+            //Adittional Loop for endless mode
+            System.out.println("Do you want to continue? (y/n)");
+            String answer = scanner.next();
+            if (answer.equals("n")) {
+                IsgoingOn = false;
+            }
+
+            }
+        }
     }
-}
